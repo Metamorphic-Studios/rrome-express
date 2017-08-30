c357dba8b479ab46925c8b1963fe2c9a37fa5953
Ross Leitch <Ross@metamorphicsolutions.com>
2017-08-30 16:16:14 +1200

    init

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express');
+var pretty = require('express-prettify');
+var cors = require('cors');
 var bodyParser = require('body-parser');
 const router = express.Router();
 
 var route = (rrome) => {
    router.use(bodyParser.json());
    router.use(bodyParser.urlencoded());
+   router.use(pretty({query: 'pretty'}));
+   router.use(cors());
 
    router.get('/models', (req, res) => {
       //JWT to define which models
+      rrome.listModels((err, models) => {
+         res.send((err) ? {error: err} : models)
+      });
    });
 
    router.get('/model/:model_id', (req, res) => {
@@ -19,6 +26,19 @@ var route = (rrome) => {
       }else{
          res.send({error: "No model id provided"});
       }
+   });
+
+   router.post('/model/:model_id', (req, res) => {
+      var id = req.params.model_id;
+      rrome.updateModel(id, req.body.model, (err, data) => {
+         res.send((err) ? {error: err} : data)
+      });
+   });
+
+   router.post('/model', (req, res) => {
+      rrome.addModel(req.body.name, req.body.model, (err, id) => {
+         res.send((err) ? {error: err} : {id: id})
+      });
    });
 
    router.get('/data/id/:id', (req, res) => {
